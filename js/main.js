d7d13f0490588db0f5bc372cff90bbdce6c30fda
maadc <47146214+maadc@users.noreply.github.com>
2019-11-11 17:38:03 +0100

    #3 beendet. Ball bewegt sicht + Testumgebung lite

---
+++ js/main.js
@@ -0,0 +1,94 @@
+let frametimeBefore = Date.now();
+let frametime; // in ms
+
+let spielfeld = document.getElementById("spielfeld");
+
+let ball = {
+    object: document.getElementById("ball"),
+    position: {
+        bottom: 350, //in px
+        left: 350, //in px
+        angle: 90, //in degrees.
+    }
+}
+
+function ballLogic(frametime) {
+        moveBall(ballPositon.angle, frametime);
+    
+}
+
+function moveBall(angle, frametime) {
+    let newBallPosition = {
+        bottom: ball.position.bottom + (getDirection(angle).y * 100 * frametime),
+        left: ball.position.left + (getDirection(angle).x * 100 * frametime),
+        angle: angle,
+    }
+    ball.style.bottom = "" + newBallPosition.bottom;
+    ball.position.bottom = newBallPosition.bottom;
+
+    ball.style.left = "" + newBallPosition.left;
+    ball.position.left = newBallPosition.left;
+}
+
+function getDirection(directAngle) {
+    let x;
+    let y;
+    let angle = (Math.PI / 180) * directAngle
+
+    if (directAngle <= 90) {
+        let xa = Math.sin(angle);
+        let ya = Math.cos(angle);
+
+        x = xa / (xa + ya);
+        y = ya / (xa + ya);
+    } else if (directAngle <= 180) {
+        let xa = Math.sin(angle);
+        let ya = Math.cos(angle);
+
+        x = xa / (xa + Math.abs(ya));
+        y = ya / (xa + Math.abs(ya));
+    } else if (directAngle <= 270) {
+        let xa = Math.sin(angle);
+        let ya = Math.cos(angle);
+
+        x = xa / Math.abs(xa + ya);
+        y = ya / Math.abs(xa + ya);
+    } else if (directAngle <= 360) {
+        let xa = Math.sin(angle);
+        let ya = Math.cos(angle);
+
+        x = xa / (Math.abs(xa) + ya);
+        y = ya / (Math.abs(xa) + ya);
+    }
+    return {
+        x: round(x),
+        y: round(y),
+    }
+}
+
+function round(n) {
+    // 2 decimal places
+    let number = (Math.round(n * 100) / 100)
+    return number;
+}
+
+function gameLoop() {
+    //in dem Loop sollen alle regelmäßigen Events aufgerufen werden.
+
+    let now = Date.now(); //gibt eine bestimmte Anzahl an Millisekunden aus. 
+    frametime = (now - frametimeBefore) * 0.001;
+    // Abhängigkeit von Frametime ist wichtig, weil selbst bei geringer Prozessorgeschwindigkeit
+    // das Spiel nicht schneller laufen soll, als der Spieler es spielen kann. 
+    // gleichzeitig bestimmt die frametime die Spielgeschwindigkeit.
+
+    ballLogic(frametime);
+
+    frametimeBefore = now;
+}
+
+setInterval(gameLoop, 0);
+
+module.exports = {
+    getDirection: getDirection,
+    round: round
+}
---
+++ js/test.js
@@ -0,0 +1,47 @@
+describe("getDirection of 4 different angles", () => {
+    const getDirection = require('./main.js').getDirection;
+    [{
+        input: 45,
+        output: {
+            x: 0.5,
+            y: 0.5
+        }
+    },
+    {
+        input: 135,
+        output: {
+            x: 0.5,
+            y: -0.5
+        }
+    },
+    {
+        input: 225,
+        output: {
+            x: -0.5,
+            y: -0.5
+        }
+    }
+    ,
+    {
+        input: 315,
+        output: {
+            x: -0.5,
+            y: 0.5
+        }
+    }
+].forEach(({input,output}) => {test(`direction of ${input}° angle`, () => {
+            expect(getDirection(input)).toEqual({
+                x: output.x,
+                y: output.y
+            })
+        })
+    })
+})
+
+describe("round", ()=>{
+    const round = require('./main.js').round;
+
+    test("round any number", () => {
+        expect(round(0.12345)).toEqual(0.12)
+})
+})
