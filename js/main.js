4141295606d4c075a2d8221345848121a1b60e83
maadc <47146214+maadc@users.noreply.github.com>
2019-11-18 20:40:38 +0100

    #1 Refactoring

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-window.onload = function () {
-    
+window.onload = function () {
+
     //TODO: Größen der Pongbars und Spielfeld sollten in einer INIT-Funktion berechnet werden
-    var pongbar_right = document.getElementById("pongbar_right");
+    const pongbar_right = document.getElementById("pongbar_right");
     pongbar_right.style.top = 250;
     pongbar_right.style.height = 200;
 
-    var pongbar_left = document.getElementById("pongbar_left");
+    const pongbar_left = document.getElementById("pongbar_left");
     pongbar_left.style.top = 250;
     pongbar_left.style.height = 200;
 
-    var spielfeld = document.getElementById("spielfeld");
-    spielfeld.style.height = 800;
+    const spielfeld = document.getElementById("spielfeld");
 
     var keysDown = {};
 
@@ -23,86 +22,82 @@ window.onload = function () {
 
     const speed = 12;
 
-    function gameLoop() {
-        
+    function checkPressedKeys() {
+
         //DOWN(40) and W(87)
-        if(40 in keysDown && 87 in keysDown) {
+        if (40 in keysDown && 87 in keysDown) {
             calculatePosition(pongbar_right, false);
             calculatePosition(pongbar_left, true);
         }
 
         //DOWN(40) AND S(83)
-        else if(40 in keysDown && 83 in keysDown) {
+        else if (40 in keysDown && 83 in keysDown) {
             calculatePosition(pongbar_right, false);
             calculatePosition(pongbar_left, false);
         }
 
         //UP(38) AND S(83)
-        else if(38 in keysDown && 83 in keysDown) {
+        else if (38 in keysDown && 83 in keysDown) {
             calculatePosition(pongbar_right, true);
             calculatePosition(pongbar_left, false);
         }
 
         //UP(38) AND W(87)
-        else if(38 in keysDown && 87 in keysDown) {
+        else if (38 in keysDown && 87 in keysDown) {
             calculatePosition(pongbar_right, true);
             calculatePosition(pongbar_left, true);
         }
 
         //Only UP(38)
-        else if(38 in keysDown) {
+        else if (38 in keysDown) {
             calculatePosition(pongbar_right, true);
         }
 
         //Only DOWN(40)
-        else if(40 in keysDown) {
+        else if (40 in keysDown) {
             calculatePosition(pongbar_right, false);
         }
 
         //Only S(83)
-        else if(83 in keysDown) {
+        else if (83 in keysDown) {
             calculatePosition(pongbar_left, false);
         }
 
         //Only W(87)
-        else if(87 in keysDown) {
+        else if (87 in keysDown) {
             calculatePosition(pongbar_left, true);
         }
 
     }
 
-    /*  CalculatePosition berechnet die neue Position der Pongbar
-        Wenn directionUp = true  :  Pongbar bewegt sich nach OBEN
-        Wenn directionUp = false :  Pongbar bewegt sich nach UNTEN
-    */
-
     function calculatePosition(pongbar, directionUp) {
+        /*  CalculatePosition berechnet die neue Position der Pongbar
+            Wenn directionUp = true  :  Pongbar bewegt sich nach OBEN
+            Wenn directionUp = false :  Pongbar bewegt sich nach UNTEN
+        */
+        const pos = parseInt(pongbar.style.top);
 
-        var pos = parseInt(pongbar.style.top);    250
-
-        if(directionUp) {
-            pos -= speed;                         238
-        }
-
-        else {
-            pos += speed;                         262
+        if (directionUp) {
+            pos -= speed;
+        } else {
+            pos += speed;
         }
 
         //Oberer Rand
-        if(pos < 0) {
-            pos = 0; 
+        if (pos < 0) {
+            pos = 0;
         }
-          
+
         //Unterer Rand
-        else if((parseInt(pongbar.style.height) + pos) > parseInt(spielfeld.style.height)) {
-            pos = spielfeld.style.height - pongbar.style.height;
+        else if ((parseInt(pongbar.style.height) + pos) > parseInt(spielfeld.offsetHeight)) {
+            pos = spielfeld.offsetHeight - pongbar.style.height;
         }
 
         pongbar.style.top = pos;
 
     }
 
-    setInterval(gameLoop, 30);
-        
+    setInterval(() => {
+        checkPressedKeys();
+    }, 30);
 };
-    
