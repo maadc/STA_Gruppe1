7e06a34435c7e181afd93c17adfc92751034ba00
maadc <47146214+maadc@users.noreply.github.com>
2019-12-08 15:52:25 +0100

    deleted dublicate pongbar-code

--- js/bundle.js
+++ js/bundle.js
@@ -277,96 +277,10 @@ window.onload = () => {
         saveBallValues(newBallPosition.left, newBallPosition.bottom, newBallPosition.angle);
     }
 
-    
-    
-
     function saveBallValues(left, bottom, angle) {
         ball.position.left = left;
         ball.position.bottom = bottom;
         ball.position.angle = angle;
-    }
-
-    window.addEventListener("keydown", function (event) {
-        keysDown[event.keyCode] = true;
-    }, false);
-    window.addEventListener("keyup", function (event) {
-        delete keysDown[event.keyCode];
-    }, false);
-
-    function checkPressedKeys() {
-
-        //DOWN(40) and W(87)
-        if (40 in keysDown && 87 in keysDown) {
-            calculatePosition(pongbar_right, false);
-            calculatePosition(pongbar_left, true);
-        }
-
-        //DOWN(40) AND S(83)
-        else if (40 in keysDown && 83 in keysDown) {
-            calculatePosition(pongbar_right, false);
-            calculatePosition(pongbar_left, false);
-        }
-
-        //UP(38) AND S(83)
-        else if (38 in keysDown && 83 in keysDown) {
-            calculatePosition(pongbar_right, true);
-            calculatePosition(pongbar_left, false);
-        }
-
-        //UP(38) AND W(87)
-        else if (38 in keysDown && 87 in keysDown) {
-            calculatePosition(pongbar_right, true);
-            calculatePosition(pongbar_left, true);
-        }
-
-        //Only UP(38)
-        else if (38 in keysDown) {
-            calculatePosition(pongbar_right, true);
-        }
-
-        //Only DOWN(40)
-        else if (40 in keysDown) {
-            calculatePosition(pongbar_right, false);
-        }
-
-        //Only S(83)
-        else if (83 in keysDown) {
-            calculatePosition(pongbar_left, false);
-        }
-
-        //Only W(87)
-        else if (87 in keysDown) {
-            calculatePosition(pongbar_left, true);
-        }
-
-    }
-
-    function calculatePosition(pongbar, directionUp) {
-        /*  CalculatePosition berechnet die neue Position der Pongbar
-            Wenn directionUp = true  :  Pongbar bewegt sich nach OBEN
-            Wenn directionUp = false :  Pongbar bewegt sich nach UNTEN
-        */
-        const speed = 12;
-        let pos = parseInt(pongbar.style.top);
-
-        if (directionUp) {
-            pos -= speed;
-        } else {
-            pos += speed;
-        }
-
-        //Oberer Rand
-        if (pos < 0) {
-            pos = 0;
-        }
-
-        //Unterer Rand
-        else if ((parseInt(pongbar.style.height) + pos) > parseInt(spielfeld.offsetHeight)) {
-            pos = spielfeld.offsetHeight - pongbar.style.height;
-        }
-
-        pongbar.style.top = pos;
-
     }
 
     function gameLoop() {
@@ -443,8 +357,6 @@ window.onload = () => {
         }
 
     }
-
-    
 
     function calculatePosition(pongbar, directionUp) {
         /*  CalculatePosition berechnet die neue Position der Pongbar
