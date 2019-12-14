998ae435acd7fce4833419f8194dd8c261d0f39d
maadc <47146214+maadc@users.noreply.github.com>
2019-12-14 21:56:35 +0100

    refactoring der ball-tests und funktionen

--- js/test/ball.test.js
+++ js/test/ball.test.js
@@ -1,63 +1,44 @@
 //Simuliere die Funktion, die aus getterDOM.js kommt
 jest.mock("../getterDOM.js");
+jest.mock("../setterDOM.js");
 //Speichere die Funktion, die aus getterDOM.js kommt
 let getterDOM = require("../getterDOM.js");
+let setterDOM = require("../setterDOM.js");
 
-
-test(`if spielfeld exists`, () => {
-    const spielfeld = document.createElement("spielfeld");
-    spielfeld.style.width = "300px";
-    expect(spielfeld.style.width).toBe("300px");
-})
-
-test(`test`, () => {
-    //Simuliere das Spielfeld 100px x 100px ist
-    getterDOM.mockImplementation(() => 100);
-    //hole das ball-Objekt
-    let ball = require("../ball.js").ball;
+describe('function: ballReset', () => {
+    test(`gamefield 300x300px`, () => {
+        //Simuliere das Spielfeld 300 x 300px ist
+        getterDOM.mockImplementation(() => 300);
+        //hole ball
+        let ball = require("../ball.js").ball;
     
-    //Erstelle ein Test-DOM
-    document.body.innerHTML =
-    '<div>' +
-    '  <div id="ball" style="left: 100px"/></div>' +
-    '</div>';
+        //hole ballReset()
+        let ballReset = require("../ball.js").ballReset;
+    
+        //Platziere den Ball in der Mitte
+        ballReset();
+        expect(ball.position.left && ball.position.bottom).toBe(150);
+    })
+});
 
-    ball.object = document.getElementById("ball");
-
-    //setze ein Stil. Hier könnte auch ein Funktionsaufruf sein.
-    ball.object.style.setProperty('left', '10px');
-
-    expect(ball.object.style.left).toBe("10px");
-})
-
-test(`ballReset`, () => {
-    //Simuliere das Spielfeld 300 x 300px ist
-    getterDOM.mockImplementation(() => 300);
-    //hole ball
-    let ball = require("../ball.js").ball;
-
-    //hole ballReset()
-    let ballReset = require("../ball.js").ballReset;
-
-    //Platziere den Ball in der Mitte
-    ballReset();
-    expect(ball.position.left && ball.position.bottom).toBe(150);
-})
-
-test("saveBallValues", () => {
-    let saveBallValues = require("../ball.js").saveBallValues;
-    let ball = require("../ball.js").ball;
-
-    saveBallValues(100, 100, 20);
-    expect(ball.position).toHaveProperty('left', 10, 'bottom', 100, 'angle', 20); //?
-
-})
-
-test("moveBall", () => {
+describe('function: SaveBallValues', () => {
+    test("Values: 100left, 10bottom, 20°", () => {
+        let saveBallValues = require("../ball.js").saveBallValues;
+        let ball = require("../ball.js").ball;
+    
+        saveBallValues(100, 10, 20);
+        expect(setterDOM).toBeCalledWith("ball", "style_left", 100);
+        expect(setterDOM).toBeCalledWith("ball", "style_bottom", 10);
+        expect(ball.position).toEqual({'left': 100, 'bottom': 10, 'angle': 20});
+    })
+});
+describe('function: moveBall', () => {
     let moveBall = require("../ball.js").moveBall;
     let ball = require("../ball.js").ball;
-    debugger;
-    moveBall(10, 0.005);
-    expect(ball.position).toHaveProperty('left', 10, 'bottom', 100, 'angle', 10);
-
-})
+    
+    test("90°, 0.005frametime", () => {
+        ball.position = {left: 150, bottom: 150, angle: 90}
+        moveBall(90, 0.005);
+        expect(ball.position).toEqual({'left': 153, 'bottom': 150, 'angle': 90});
+    })
+});
