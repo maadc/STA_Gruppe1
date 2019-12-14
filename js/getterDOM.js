4fe9bc6f0ece4a3049831dd70b9494d5acda64a1
maadc <47146214+maadc@users.noreply.github.com>
2019-12-14 20:25:05 +0100

    pongbar_left/_right.style.top

--- js/getterDOM.js
+++ js/getterDOM.js
@@ -1,19 +1,22 @@
 module.exports = (Kommando) => {
 
-        let spielfeld = document.getElementById("spielfeld");
-        let pongbar_right = document.getElementById("pongbar_right");
-        let pongbar_left = document.getElementById("pongbar_left");
+    let spielfeld = document.getElementById("spielfeld");
+    let pongbar_right = document.getElementById("pongbar_right");
+    let pongbar_left = document.getElementById("pongbar_left");
 
-        if (Kommando === "spielfeld.offsetHeight"){
-            return spielfeld.offsetHeight;
-        } else if (Kommando === "spielfeld.offsetWidth") {
-            return spielfeld.offsetWidth;
-        } else if (Kommando === "spielfeld") {
-            return spielfeld;
-        } else if (Kommando === "pongbar_right") {
-            return pongbar_right;
-        } else if (Kommando==="pongbar_left") {
-            return pongbar_left;
-        }
-
+    if (Kommando === "spielfeld.offsetHeight"){
+        return spielfeld.offsetHeight;
+    } else if (Kommando === "spielfeld.offsetWidth") {
+        return spielfeld.offsetWidth;
+    } else if (Kommando === "spielfeld") {
+        return spielfeld;
+    } else if (Kommando === "pongbar_right") {
+        return pongbar_right;
+    } else if (Kommando==="pongbar_left") {
+        return pongbar_left;
+    } else if (Kommando === "pongbar_right.style.top") {
+        return pongbar_right.style.top;
+    } else if (Kommando==="pongbar_left.style.top") {
+        return pongbar_left.style.top;
+    }
 }
