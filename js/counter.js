4a6e61aed7e3498a0932ae8cc5a5441df42c87a2
Lena Reulein <you@example.com>
2019-12-16 15:21:48 +0100

    Testing Counter with Luis

--- js/counter.js
+++ js/counter.js
@@ -1,18 +1,4 @@
 let getterDOM = require("./getterDOM.js");
-/*
-const countPointRight = () => {
-    let score_right = getterDOM("punktestandLinks");
-    let scoreRight = parseInt(score_right);
-    score_right = scoreRight + 1;
-    return score_right;
-}
-
-const countPointLeft = () => {
-    let score_left = getterDOM("punktestandRechts");
-    let scoreLeft = parseInt(score_left);
-    score_left = scoreLeft + 1;
-    return score_left;
-}*/
 
 module.exports = {
     countPointRight: countPointRight,
