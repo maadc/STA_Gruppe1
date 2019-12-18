cc61afe03dd5f977fc1f5cc87acb75d04e511bc1
Elisabeth <elisabeth.thirmeyer@web.de>
2019-12-18 15:40:15 +0100

    TestsTimercomplete

--- js/test/timer.test.js
+++ js/test/timer.test.js
@@ -8,7 +8,22 @@ test("Function: timerRunning", () => {
     expect(startCounter()).toBe(true);
 })
 
+describe('function: updateCounter', () => {
+    test("sec should be +1", () => {
+        expect(updateCounter(1000)).toEqual({"min": "00", "sec": "01"});
+    })
 
-test("Function: updateCounter", () => {
-    expect(updateCounter(1000)).toEqual({"min": "00", "sec": "01"});
+    test("min should be +1", () => {
+        expect(updateCounter(60000)).toEqual({"min": "01", "sec": "00"});
+    })
+
+    test("min should be +1 and sec should be +30", () => {
+        expect(updateCounter(90000)).toEqual({"min": "01", "sec": 30});
+    })
+});
+
+/* test("Function: setTime", () => {
+    getterDOM.mockImplementation( ()=> document.createElement("tracker") )
+    expect(tracker).toEqual({"Min:"});
 })
+*/
