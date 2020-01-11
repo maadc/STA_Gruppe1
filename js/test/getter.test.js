3b9456983b5a119066753b243f6e2cd6506b0926
maadc <47146214+maadc@users.noreply.github.com>
2020-01-11 12:10:18 +0100

    first test = pass

--- js/test/getter.test.js
+++ js/test/getter.test.js
@@ -1,7 +1,6 @@
 let getterDOM = require("../getterDOM.js");
 
 describe("function: getterDOM", () => {
-    console.log(htmlDoc);
     //simuliere die HTML-Struktur unserer Index-HTML
     document.body.innerHTML =
     `<div id="spielfeld"></div>`;
