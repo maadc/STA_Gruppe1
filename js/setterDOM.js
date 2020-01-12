895a8d93b50520ed20c4984b4640c6c7f76f96c1
maadc <47146214+maadc@users.noreply.github.com>
2020-01-12 20:04:16 +0100

    setter-tests

--- js/setterDOM.js
+++ js/setterDOM.js
@@ -1,28 +1,57 @@
-
-module.exports = (object,parameter, value) => {
+module.exports = (object, parameter, value) => {
     let ball = document.getElementById("ball");
     let pongbar_right = document.getElementById("pongbar_right");
     let pongbar_left = document.getElementById("pongbar_left");
+    let starttext = document.getElementById("starttext")
 
-    if (object === "ball"){
-        if(parameter === "style_left"){
-            ball.style.left = value;
+    if (object === "ball") {
+        if (parameter === "style_left") {
+            if (typeof value === "number") {
+                ball.style.left = value + "px";
+            } else {
+                ball.style.left = value;
+            }
+            return ball.style.left;
         }
-        if(parameter === "style_bottom"){
-            ball.style.bottom = value;
+        
+        if (parameter === "style_bottom") {
+            if (typeof value === "number") {
+                ball.style.bottom = value + "px";
+            } else {
+                ball.style.bottom = value;
+            }
+            return ball.style.bottom;
         }
     }
 
-    if (object === "pongbar_left"){
-        if (parameter === "style_top"){
-            pongbar_left.style.top = value;
+    if (object === "pongbar_left") {
+        if (parameter === "style_top") {
+            if (typeof value === "number") {
+                pongbar_left.style.top = value + "px";
+            } else {
+                pongbar_left.style.top = value;
+            }
+            return pongbar_left.style.top;
         }
     }
 
-    if (object === "pongbar_right"){
-        if (parameter === "style_top"){
-            pongbar_right.style.top = value;
+    if (object === "pongbar_right") {
+        if (parameter === "style_top") {
+            if (typeof value === "number") {
+                pongbar_right.style.top = value + "px";
+            } else {
+                pongbar_right.style.top = value;
+            }
+            return pongbar_right.style.top;
         }
     }
-   
+
+    if (object === "starttext" && parameter === "innerHTML"){
+        if (typeof value != "string"){
+            return undefined;
+        }
+        starttext.innerHTML = value;
+        return starttext.innerHTML;
+    }
+
 }
