8cbe3382b5f8457dc94cdc1952671733ef3a2956
LuisRuckriegel <Luis.Ruckriegel@gmx.de>
2020-01-16 12:04:24 +0100

    tested sound.js

--- js/sound.js
+++ js/sound.js
@@ -3,12 +3,16 @@ let getterDOM = require("./getterDOM.js");
 function playSound (kommando) {
     if (kommando === "soundAbprallen") {
         getterDOM("soundAbprallen").play();
+        return soundAbprallen;
     } else if (kommando === "soundPunkt") {
         getterDOM("soundPunkt").play();
+        return soundPunkt;
     } else if (kommando === "soundStart") {
         getterDOM("soundStart").play();
+        return soundStart;
     } else if (kommando === "soundHintergrund") {
         getterDOM("soundHintergrund").play();
+        return soundHintergrund;
     }
 }
 
---
+++ js/test/sound.test.js
@@ -0,0 +1,32 @@
+let playSound = require("../sound.js").playSound;
+
+describe("function: playSound", () => {
+    //simuliere die HTML-Struktur aus unserer index.html
+    document.body.innerHTML = `
+    <audio id="soundAbprallen" preload="auto" >
+        <source src="../sound/soundAbprallen.mp3" type="audio/mpeg" />
+    </audio>
+    <audio id="soundPunkt" preload="auto">
+        <source src="../sound/soundPunkt.mp3" type="audio/mpeg" />
+    </audio>
+    <audio id="soundStart" preload="auto">
+        <source src="../sound/soundStart.mp3" type="audio/mpeg" />
+    </audio>
+    <audio loop id="soundHintergrund" preload="auto">
+        <source src="../sound/soundHintergrund.mp3" type="audio/mpeg" />
+    </audio> 
+    `; 
+
+    test("soundAbprallen", () => {
+        expect(playSound("soundAbprallen")).toBe(soundAbprallen)
+    })
+    test("soundStart", () => {
+        expect(playSound("soundStart")).toBe(soundStart)
+    })
+    test("soundHintergrund", () => {
+        expect(playSound("soundHintergrund")).toBe(soundHintergrund)
+    })
+    test("soundPunkt", () => {
+        expect(playSound("soundPunkt")).toBe(soundPunkt)
+    })
+})
