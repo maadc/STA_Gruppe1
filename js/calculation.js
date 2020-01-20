46851d1e435113e1e7b92ffb1be62f43fe54826a
maadc <47146214+maadc@users.noreply.github.com>
2020-01-20 23:09:24 +0100

    calcRandomAngle: dont spawn with the horizontal angel

--- js/calculation.js
+++ js/calculation.js
@@ -97,9 +97,15 @@ function round(n) {
 
 function calcRandomAngle() { //Zufälliger Winkel, der senkrechte Winkel ausschließt.
    let randomAngle; 
-    do
+    do {
         randomAngle = Math.floor(Math.random() * 360);
-    while (randomAngle < 30 || (randomAngle > 160 && randomAngle < 200) || randomAngle > 330);
+        console.log("error:"+randomAngle);
+    }
+    while (randomAngle < 30 || 
+        (randomAngle > 150 && randomAngle < 210) || 
+        (randomAngle > 80 && randomAngle < 100) || 
+        (randomAngle > 260 && randomAngle < 280) || 
+        randomAngle > 330);
     return randomAngle;
 }
 
